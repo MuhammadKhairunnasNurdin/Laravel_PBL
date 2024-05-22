2febfc1344e0056ef98c8906f3a763b2e7f11ba4
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2024-05-23 03:58:40 +0700

    Feat(Data Penduduk): add implementing view page for CRUD data penduduk feature

---
+++ resources/views/admin/penduduk/edit.blade.php
@@ -0,0 +1,182 @@
+@extends('admin.layouts.template')
+
+@section('content')
+    <form action="{{url('admin/penduduk/' . $penduduk->penduduk_id)}}" method="post">
+        @csrf
+        <!--add 'PUT' method, because we use Route::put() for update-->
+        {!! method_field('PUT') !!}
+
+        <input type="hidden" name="penduduk" value="{{json_encode($penduduk->toArray())}}">
+
+        <div class="flex flex-col bg-white  mx-5 my-5 shadow-[0_-4px_0_0_rgba(29,78,216,1)] rounded-md">
+            <div class="flex justify-between items-center w-full py-2 border-b">
+                <p class="text-lg mx-10">Ubah Data Penduduk</p>
+            </div>
+
+            <div class="grid md:grid-cols-2 my-[30px] mx-10 gap-x-[101px]">
+
+                {{-- KOLOM KANAN --}}
+                <div class="md:col-span-1 flex flex-col gap-[23px] order-2">
+
+                    {{-- PENDIDIKAN --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Pendidikan<span class="text-red-400">*</span></p>
+                        <select name="pendidikan" id="pendidikan" class="w-100 border border-stone-400 text-sm font-normal pl-[10px] py-[10px] rounded-[5px] focus:outline-none">
+                            @php
+                                $options = ['Belum Sekolah', 'Tidak Terpelajar', 'SD', 'SMP', 'SMA/SMK', 'D4/S1', 'S2 Keatas'];
+                            @endphp
+                            @foreach ($options as $option)
+                                <option value="{{ $option }}" {{ old('pendidikan', $penduduk->pendidikan) == $option ? 'selected' : '' }}>{{ $option }}</option>
+                            @endforeach
+                        </select>
+                        @error('pendidikan')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END PENDIDIKAN --}}
+
+                    {{-- HUBUNGAN KELUARGA --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Hubungan Keluarga<span class="text-red-400">*</span></p>
+                        <select name="hubungan_keluarga" id="hubungan_keluarga" class="w-100 border border-stone-400 text-sm font-normal pl-[10px] py-[10px] rounded-[5px] focus:outline-none">
+                            @php
+                                $options = ['Kepala Keluarga', 'Istri', 'Anak'];
+                            @endphp
+                            @foreach ($options as $option)
+                                <option value="{{ $option }}" {{ old('hubungan_keluarga', $penduduk->hubungan_keluarga) == $option ? 'selected' : '' }}>{{ $option }}</option>
+                            @endforeach
+                        </select>
+                        @error('hubungan_keluarga')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END HUBUNGAN KELUARGA --}}
+
+                    {{-- RT --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">RT<span class="text-red-400">*</span></p>
+                        <select name="RT" id="RT" class="w-100 border border-stone-400 text-sm font-normal pl-[10px] py-[10px] rounded-[5px] focus:outline-none">
+                            @php
+                                $options = ['RT 01', 'RT 02', 'RT 03', 'RT 04', 'RT 05', 'RT 06'];
+                            @endphp
+                            @foreach ($options as $option)
+                                <option value="{{ $option }}" {{ old('RT', $penduduk->RT) == $option ? 'selected' : '' }}>{{ $option }}</option>
+                            @endforeach
+                        </select>
+                        @error('RT')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END RT --}}
+
+                    {{-- ALAMAT --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Alamat<span class="text-red-400">*</span></p>
+                        <textarea class="text-sm font-normal border border-stone-400 px-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" name="alamat" id="alamat" rows="1" maxlength="200" required>{{old('alamat', $penduduk->alamat)}}</textarea>
+                        @error('alamat')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END ALAMAT --}}
+
+                    {{-- NOMOR TELEPON --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Nomor Telepon</p>
+                        <input type="number" value="{{old('no_telp',$penduduk->no_telp)}}" name="no_telp" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" placeholder="Masukkan Nomor Telepon">
+                        @error('no_telp')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END NOMOR TELEPON --}}
+
+                </div>
+                {{-- END KOLOM KANAN --}}
+
+                {{-- KOLOM KIRI --}}
+                <div class="md:col-span-1 flex flex-col gap-[23px] max-md:mt-[23px] order-1">
+
+
+                    {{-- NAMA --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Nama<span class="text-red-400">*</span></p>
+                        <input type="text" step="any" value="{{old('nama', $penduduk->nama)}}" name="nama" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" placeholder="Masukkan Nama Penduduk" required>
+                        @error('nama')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END NAMA --}}
+
+                    {{-- NIK --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">NIK<span class="text-red-400">*</span></p>
+                        <input type="number" value="{{old('NIK', $penduduk->NIK)}}" name="NIK" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" placeholder="Masukkan NIK" required>
+                        @error('NIK')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END NIK --}}
+
+                    {{-- NKK --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">NKK<span class="text-red-400">*</span></p>
+                        <input type="number" name="NKK" value="{{old('NKK', $penduduk->NKK)}}" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" placeholder="Masukkan NKK" required>
+                        @error('NKK')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END NKK --}}
+
+                    {{-- TANGGAL LAHIR --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Tanggal Lahir<span class="text-red-400">*</span></p>
+                        <input type="date" step="any" name="tgl_lahir" value="{{old('tgl_lahir', $penduduk->tgl_lahir)}}" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none" id="tanggal" required>
+                        @error('tgl_lahir')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END TANGGAL LAHIR --}}
+
+                    {{-- PENDAPATAN --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Pendapatan<span class="text-red-400">*</span></p>
+                        <select name="pendapatan" id="pendapatan" class="w-100 border border-stone-400 text-sm font-normal pl-[10px] py-[10px] rounded-[5px] focus:outline-none">
+                            @php
+                                $options = ['Belum Bekerja', 'Rp 0 - Rp 500.000', 'Rp 500.000 - Rp 1.000.000', 'Rp 1.000.000 - Rp 2.000.000', 'Rp 2.000.000 - Rp 3.000.000', 'Rp 3.000.000 - Keatas'];
+                            @endphp
+                            @foreach ($options as $option)
+                                <option value="{{ $option }}" {{ old('pendapatan', $penduduk->pendapatan) == $option ? 'selected' : '' }}>{{ $option }}</option>
+                            @endforeach
+                        </select>
+                        @error('pendapatan')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END PENDAPATAN --}}
+
+                    {{-- JENIS KELAMIN --}}
+                    <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
+                        <p class="text-base text-neutral-950 pr-[10px]">Jenis Kelamin<span class="text-red-400">*</span></p>
+                        <div class="flex items-center gap-10">
+                            <input type="radio" class="indeterminate:outline-2 indeterminate:outline-stone-400 indeterminate:w-4 indeterminate:py-[6px] checked:w-4 checked:outline-2 checked:border-transparent -mr-[25px]" name="jenis_kelamin" value="L" id="option1" required {{ old('jenis_kelamin', $penduduk->jenis_kelamin) === 'L' ? 'checked' : '' }}><span>Laki-laki</span>
+                            <input type="radio" class="indeterminate:outline-2 indeterminate:outline-stone-400 indeterminate:w-4 indeterminate:py-[6px] checked:w-4 checked:outline-2 checked:bg-red-400 -mr-[25px]" name="jenis_kelamin" value="P" id="option2" required {{ old('jenis_kelamin', $penduduk->jenis_kelamin) === 'P' ? 'checked' : '' }}><span>Perempuan</span>
+                        </div>
+                        @error('jenis_kelamin')
+                        <span class="text-red-500">{{$message}}</span>
+                        @enderror
+                    </div>
+                    {{-- END JENIS KELAMIN --}}
+
+                </div>
+                {{-- END KOLOM KIRI --}}
+            </div>
+            <div class="grid md:grid-cols-2 mx-10 gap-x-[101px] pb-[30px]">
+                <div class="col-span-2 flex justify-end items-center gap-[26px] w-full" id="">
+                    <p class="text-xs"><span class="text-red-400">*</span>Wajib diisi</p>
+                    <a href="{{url('admin/penduduk' . session('urlPagination'))}}" class="bg-gray-300 text-neutral-950 font-bold text-base py-[5px] px-[19px] rounded-[5px]" id="page_1">Kembali</a>
+                    <button type="submit" class="bg-blue-700 text-white font-bold text-base py-[5px] px-[19px] rounded-[5px]" id="page_2">Simpan Data</button>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
+
--- resources/views/admin/penduduk/index.blade.php
+++ resources/views/admin/penduduk/index.blade.php
@@ -3,43 +3,32 @@
 @section('content')
     <div class="flex flex-col bg-white mx-5 my-5 shadow-[0_-4px_0_0_rgba(29,78,216,1)] rounded-md">
         <div class="flex justify-between items-center w-full py-2 border-b">
-            <p class="text-sm md:text-lg ml-5 md:ml-10">Daftar pemeriksaan lansia</p>
-            <a href="{{ url('admin/penduduk/create') }}" class="bg-blue-700 text-sm text-white font-bold py-1 px-4 mr-5 md:mr-10 rounded">Tambah</a>
+            <p class="text-sm md:text-lg ml-5 md:ml-10">Daftar Penduduk</p>
+            <a href="{{ route('penduduk.create') }}" class="bg-blue-700 text-sm text-white font-bold py-1 px-4 mr-5 md:mr-10 rounded">Tambah</a>
         </div>
         {{-- <div class="flex mt-[30px] mx-10 "> --}}
             <div class="flex w-fit h-full items-center align-middle gap-[20px] mx-10 mt-[30px]">
                 <x-dropdown.dropdown-filter>Filter</x-dropdown.dropdown-filter>
                 <x-input.search-input name="search" placeholder="Cari nama anggota posyandu"></x-input.search-input>
             </div>
-
-        @php
-            $relationships = ['penduduk', 'pemeriksaan_lansia'];
-        @endphp
-        @foreach($relationships as $relationship)
-            <input type="hidden" name="relationships[]" id="relationship" value="{{encrypt($relationship)}}">
-        @endforeach
-        <input type="hidden" name="model" id="model" value="{{encrypt('App\Models\Pemeriksaan')}}">
-        <input type="hidden" name="url" id="url" value="{{encrypt('/kader/lansia/')}}">
-        <input type="hidden" name="filterName" id="filterName" value="{{encrypt('NIK')}}">
-        <input type="hidden" name="where" id="whereName" value="{{encrypt('golongan')}}">
-        <input type="hidden" name="where" id="whereValue" value="{{encrypt('lansia')}}">
+            @if(session('success'))
+                <div class="flex items-center p-1 mb-1 border-2 border-green-500 bg-green-100 text-green-700 rounded-md" id="message">
+                    <p class="mr-4"> <b>BERHASIL</b> {{ session('success') }}</p>
+                    <button id="close" class="ml-auto bg-transparent text-green-700 hover:text-green-900">
+                        <span>&times;</span>
+                    </button>
+                </div>
+            @elseif(session('error'))
+                <div class="flex items-center p-4 mb-4 border-2 border-red-500 bg-red-100 text-red-700 rounded-md" id="message">
+                    <p class="mr-4">{{ session('error') }}</p>
+                    <button id="close" class="ml-auto bg-transparent text-red-700 hover:text-red-900">
+                        <span>&times;</span>
+                    </button>
+                </div>
+            @endif
 
         <div class="mx-10 my-[30px]">
-            {{-- <table class=" border-collapse w-full rounded-t-[10px] overflow-hidden" id="lansia_table">
-                <thead class="bg-gray-200 border-b text-left py-5">
-                    <tr class=" text-stone-400 rounded-lg">
-                        <th class="font-normal text-sm py-2">Nama</th>
-                        <th class="font-normal text-sm py-2">Tgl Pemeriksaan</th>
-                        <th class="font-normal text-sm py-2">Usia</th>
-                        <th class="font-normal text-sm py-2">Berat</th>
-                        <th class="font-normal text-sm py-2">Tinggi</th>
-                        <th class="font-normal text-sm py-2">L.Perut</th>
-                        <th class="font-normal text-sm py-2">Status</th>
-                        <th class="font-normal text-sm py-2">Aksi</th>
-                    </tr>
-                </thead>
-            </table> --}}
-            <x-table.data-table :dt="$penduduks" 
+            <x-table.data-table :dt="$penduduks"
                                 :headers="['Nama', 'NIK', 'NKK', 'Tanggal Lahir', 'Jenis Kelamin', 'Hubungan Keluarga', 'Aksi']">
                 @php
                     $no = ($penduduks->currentPage() - 1) * $penduduks->perPage() + 1;
@@ -49,16 +38,20 @@
                         <td class="px-6 border-b lg:py-2 bg-white">{{$pd->nama}}</td>
                         <td class="px-6 lg:py-2 border-b bg-white">{{$pd->NIK}}</td>
                         <td class="px-6 lg:py-2 border-b bg-white">{{$pd->NKK}}</td>
-                        <td class="px-6 lg:py-2 border-b bg-white">{{$pd->tgl_lahir}}</td>
+                        <td class="px-6 lg:py-2 text-nowrap border-b bg-white">{{$pd->tgl_lahir}}</td>
                         <td class="px-6 lg:py-2 border-b bg-white">{{$pd->jenis_kelamin}}</td>
-                            {{-- @dd($pd->pemeriksaan_lansia->lingkar_perut); --}}
                         <td class="px-6 lg:py-2 border-b bg-white">{{$pd->hubungan_keluarga}}</td>
                         <td class="px-6 lg:py-2 border-b bg-white">
-                            <form action="lansia/{{$pd->penduduk_id}}" method="post" class="flex items-center gap-2">
-                                <a href="lansia/{{$pd->penduduk_id}}" class="bg-blue-400 text-[12px] text-neutral-950 py-[5px] px-2 rounded-sm hover:bg-blue-600 hover:text-white">Detail</a>
-                                <a href="lansia/{{$pd->penduduk_id}}/edit" class="bg-yellow-400 text-[12px] text-neutral-950 py-[5px] px-2 rounded-sm hover:bg-yellow-300">Ubah</a>
+                            <form action="penduduk/{{$pd->penduduk_id}}" method="post" class="flex items-center gap-2">
+                                @php
+                                    $queryString = http_build_query(request()->query());
+                                    session(['urlPagination' => $queryString ? '?' . $queryString : '']);
+                                @endphp
+                                <a href="penduduk/{{$pd->penduduk_id}}" class="bg-blue-400 text-[12px] text-neutral-950 py-[5px] px-2 rounded-sm hover:bg-blue-600 hover:text-white">Detail</a>
+                                <a href="penduduk/{{$pd->penduduk_id}}/edit" class="bg-yellow-400 text-[12px] text-neutral-950 py-[5px] px-2 rounded-sm hover:bg-yellow-300">Ubah</a>
                                 @csrf
                                 @method('DELETE')
+                                <input type="hidden" name="updated_at" value="{{ $pd->updated_at }}">
                                 <button type="submit" onclick="return confirm('Apakah anda yakin ingin menghapus data?')" class="bg-red-400 text-[12px] text-neutral-950 py-[6px] px-2 rounded-sm hover:bg-red-600 hover:text-white">Hapus</button>
                             </form>
                         </td>
@@ -77,22 +70,22 @@
     function clearTable() {
         const table = document.getElementById('dataTable');
         const rows = table.getElementsByTagName('tr');
-    
+
         for (let i = rows.length - 1; i > 0; i--) {
             table.deleteRow(i);
         }
     }
-    
+
     function addRowToTable(item) {
         const table = document.getElementById('dataTable');
         const row = table.insertRow(-1);
-        
+
         row.innerHTML = `
         <x-table.table-row>
                     <td class="px-6 border-b lg:py-2 bg-white">${item.nama}</td>
                     <td class="px-6 lg:py-2 border-b bg-white">${item.NIK}</td>
                     <td class="px-6 lg:py-2 border-b bg-white">${item.NKK}</td>
-                    <td class="px-6 lg:py-2 border-b bg-white">${item.tgl_lahir}</td>
+                    <td class="px-6 lg:py-2 text-nowrap border-b bg-white">${item.tgl_lahir}</td>
                     <td class="px-6 lg:py-2 border-b bg-white">${item.jenis_kelamin}</td>
                     <td class="px-6 lg:py-2 border-b bg-white">${item.hubungan_keluarga}</td>
                     <td class="px-6 lg:py-2 border-b bg-white">
@@ -107,12 +100,12 @@
         </x-table.table-row>
     `;
     }
-    
+
     async function searchFunction() {
         let input;
         input = document.getElementById('searchInput');
         search = input.value;
-    
+
         try {
             // Make a request to the server
             const response = await fetch(`/api/penduduk/search?search=${search}`, {
@@ -122,27 +115,38 @@
                     'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
                 },
             });
-    
+
             const responseData = await response.json();
-    
+
             clearTable();
-    
+
             responseData[0].data.forEach(item => {
                 addRowToTable(item);
             });
-    
-    
+
+
         } catch (error) {
             console.log(error);
             const table = document.getElementById('dataTable');
-    
+
             clearTable();
-    
+
             const row = table.insertRow(-1);
             row.innerHTML = `
                 <td colspan="7" class="text-center p-6 bg-white border-b font-medium text-Neutral/60">Data tidak ditemukan</td>
                 `;
         }
     }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        let div = document.getElementById('message');
+        let button = document.getElementById('close');
+
+        if (div && button) {
+            button.addEventListener('click', function() {
+                div.classList.add('hidden');
+            });
+        }
+    });
 </script>
 @endpush
