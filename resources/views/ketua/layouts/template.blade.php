9339680d1290042971140b37c00882760e40ddae
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2024-05-23 13:01:53 +0700

    fix (layout): fix sidebar for all page

--- resources/views/ketua/layouts/template.blade.php
+++ resources/views/ketua/layouts/template.blade.php
@@ -20,9 +20,10 @@
 <body class="bg-gray-200 w-full md:h-screen">
     @include('ketua.layouts.header')
     <div class="flex lg:grid lg:grid-cols-6 h-full mt-[50px] md:mt-[88px]">
-        <div id="sidebar" class="fixed lg:static z-20 lg:z-auto -translate-x-[999px] transform transition ease-in-out duration-500 sm:duration-700 md:col-span-1 lg:translate-x-0 ">
+        <div id="sidebar" class="fixed z-20 h-full -translate-x-[999px] transform transition ease-in-out duration-500 sm:duration-700 col-span-1 lg:translate-x-0 ">
             @include('ketua.layouts.sidebar')
         </div>
+        <div class="col-span-1 hidden lg:flex"></div>
         <div class="w-full md:col-span-5">
             <div class="flex text-base px-5 md:text-2xl pt-5">
                 {{-- @include('kader.layouts.breadcrumb') --}}
