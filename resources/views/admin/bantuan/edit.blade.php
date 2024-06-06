0e59a12505f5962fe65f834559ce68bb0ae52abf
Lukman Eka Septiawan <lukmaneka216@gmail.com>
2024-06-06 23:18:52 +0700

    Feat: delete button tambah & delete, change input type, update bobot kriteria

--- resources/views/admin/bantuan/edit.blade.php
+++ resources/views/admin/bantuan/edit.blade.php
@@ -7,7 +7,7 @@
         </div>
             @if(session('success'))
                 <div class="flex items-center p-1 mb-1 border-2 border-green-500 bg-green-100 text-green-700 rounded-md" id="message">
-                    <p class="mr-4"> <b>BERHASIL</b> {{ session('success') }}</p>
+                    <p class="mr-4">{{ session('success') }}</p>
                     <button id="close" class="ml-auto bg-transparent text-green-700 hover:text-green-900">
                         <span>&times;</span>
                     </button>
@@ -21,7 +21,6 @@
                 </div>
             @endif
 
-        {{-- <div class="grid lg:grid-cols-3 mx-10 my-[30px]"> --}}
         <form action="{{url('admin/kriteria/' . $kriteria->kode)}}" method="post">
             @csrf
             {!! method_field('PUT') !!}
@@ -32,7 +31,7 @@
                     {{-- KODE KRITERIA --}}
                     <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
                         <p class="text-base text-neutral-950 pr-[10px]">Kode Kriteria<span class="text-red-400">*</span></p>
-                        <input type="text" step="any" name="kode" value="{{old('kode', $kriteria->kode)}}" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" placeholder="Masukkan kode kriteria awali dengan huruf C" disabled>
+                        <input type="text" step="any" name="kode" value="{{old('kode', $kriteria->kode)}}" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300 border-0" placeholder="Masukkan kode kriteria awali dengan huruf C" disabled>
                         @error('kode')
                             <span class="text-red-500 message">{{$message}}</span>
                         @enderror
@@ -60,56 +59,20 @@
                     {{-- BOBOT KRITERIA --}}
                     <div class="flex flex-col w-full h-fill gap-[20px] " id="page_1">
                         <p class="text-base text-neutral-950 pr-[10px]">Jenis Kriteria<span class="text-red-400">*</span></p>
-                        <input type="text" step="any" name="jenis" value="{{old('jenis', $kriteria->jenis)}}" class="w-100 text-sm font-normal border-stone-400 pl-[10px] py-[10px] rounded-[5px] focus:outline-none placeholder:text-gray-300" placeholder="Masukkan jenis kriteria" required>
+                        <select name="jenis" id="jenis">
+                            @php
+                                $options = ['benefit', 'cost']
+                            @endphp
+                            @foreach ($options as $option)
+                                <option value="{{$option}}" {{old('jenis', $kriteria->jenis) === $option ? 'selected' : ''}}>{{$option}}</option>
+                            @endforeach
+                        </select>
                         @error('jenis')
                             <span class="text-red-500 message">{{$message}}</span>
                         @enderror
                     </div>
                 </div>
             </div>
-            {{-- <div class="flex flex-col col-span-3 lg:col-span-1">
-                <table class="w-fit h-fit">
-                    <tbody>
-                        <tr>
-                            <td>Kode Kriteria</td>
-                            <td>:</td>
-                            <td><input type="text" name="" id=""></td>
-                        </tr>
-                        <tr>
-                            <td>Nama Kriteria</td>
-                            <td>:</td>
-                            <td><input type="text" name="" id=""></td>
-                        </tr>
-                        <tr>
-                            <td>Bobot Kriteria</td>
-                            <td>:</td>
-                            <td><input type="text" name="" id=""></td>
-                        </tr>
-                        <tr>
-                            <td>Jenis Kriteria</td>
-                            <td>:</td>
-                            <td><input type="text" name="" id=""></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div> --}}
-            {{-- <div class="flex flex-col col-span-3 lg:col-span-2 w-full rounded-2xl overflow-x-auto">
-                <x-table.data-table :dt="$rentangs"
-                                    :headers="['Kode Kriteria', 'Rentang Minimal', 'Rentang Maximal', 'Nilai']">
-                    @foreach ($rentangs as $rtg)
-                        <x-table.table-row>
-                            <td class="tableBody">{{$rtg->kode}}</td>
-                            <td class="tableBody">{{number_format($rtg->rentang_min, 3)}}</td>
-                            <td class="tableBody">{{number_format($rtg->rentang_max, 3)}}</td>
-                            <td class="tableBody">{{$rtg->nilai}}</td>
-                        </x-table.table-row>
-                    @endforeach
-                </x-table.data-table>
-            </div> --}}
-        {{-- </div> --}}
-            {{-- <div class="flex justify-end items-center w-full py-5 px-3 border-b">
-                <a href="{{ url('admin/bantuan') }}" class="bg-gray-300 text-sm text-black font-bold py-2 px-4 mr-5 md:mr-10 rounded">Kembali</a>
-            </div> --}}
             <div class="grid md:grid-cols-2 mx-10 gap-x-[101px] pb-[30px]">
                 <span id="page_1" class="col-span-1 hidden md:hidden"></span>
                 <div class="col-span-2 flex justify-end items-center gap-[26px] pt-10 w-full" id="">
